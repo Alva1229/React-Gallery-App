2c215a0aba7dae1823742ff364695dfb69055d88
ealvarez <emilioa84@hotmail.com>
2018-09-13 21:37:40 -0700

    commit

--- gallery-app/src/App.js
+++ gallery-app/src/App.js
@@ -12,7 +12,7 @@ import apiKey from './config';
 class App extends Component {
 
   componentDidMount(){
-    axios.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=20&format=json&nojsoncallback=1')
+    axios.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=`${apiKey}`&tags=${query}&per_page=20&format=json&nojsoncallback=1')
       .then(response => {
         console.log(response);
       })
